6e6441633d70d96b01d35a5372f052bcc71def95
sahar-faregh <www.sahar.fareghbal@gmail.com>
2023-02-18 09:23:31 +0330

    add-update

--- src/App.js
+++ src/App.js
@@ -3,28 +3,60 @@ import './App.css';
 import {Header} from './components/header/header'
 import {MainLayout} from './components/main-layout/main-layout'
 import {ProductsContext} from './context/products-context'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {productGirl} from './datas/girls/datas-girls'
 import {productBoy} from './datas/boys/datas-boys'
+import axios from 'axios'
+import { API_URL } from './constants/constants';
 function App() {
-  const[productGirlApp,setproductGirlApp]=useState([...productGirl])
-  const[productBoyApp,setproductBoyApp]=useState([...productBoy])
-  const[allproductsApp,setallproductsApp]=useState([...productGirl,...productBoy])
+  //const[productGirlApp,setproductGirlApp]=useState([...productGirl])
+  //const[productBoyApp,setproductBoyApp]=useState([...productBoy])
+  //const[allproductsApp,setallproductsApp]=useState([...productGirl,...productBoy])
+
+  const[productGirlApp,setproductGirlApp]=useState([])
+  const[productBoyApp,setproductBoyApp]=useState([])
+  const[allproductsApp,setallproductsApp]=useState([])
+  
   const[isGirls,setisGirls]=useState(false)
   const[isBoys,setisBoys]=useState(false) 
   
   const[genderStatus,setgenderStatus]=useState('all')
+  //---------  category -----
+  //const categoryRepeated=allproductsApp.map(pr=>pr.category)
+  //const allCategories=['all', ...new Set(categoryRepeated)]
+  const[allCategoriesApp,setallCategoriesApp]=useState([])
+  //console.log(allCategoriesApp)
 
-  const categoryRepeated=allproductsApp.map(pr=>pr.category)
-  const allCategories=['all',...new Set(categoryRepeated)]
- 
-  const[allCategoriesApp,setallCategoriesApp]=useState(allCategories)
-
-
+  //----------- maincategory ------------------
   const maincategoryRepeated=allproductsApp.map(pr=>pr.maincategory)
-  const allMaincategoryApp=['all',...new Set(maincategoryRepeated)]
-  const[maincategoryApp,setmaincategoryApp]=useState(allMaincategoryApp) 
-  console.log(maincategoryApp)
+  //console.log(maincategoryRepeated) 
+  const allMaincategories=['all',...new Set(maincategoryRepeated)]
+  //console.log(allMaincategories)
+  const[allMaincategoriesApp,setallMaincategoriesApp]=useState(allMaincategories) 
+  //console.log(allMaincategoriesApp)
+
+  //----------- subcategory -----
+  const subcategoryRepeated=allproductsApp.map(pr=>pr.subcategory)
+  //console.log(subcategoryRepeated)
+  const allSubCategories=['all',...new Set(subcategoryRepeated)]
+  //console.log(allSubCategories)
+  const[allSubCategoriesApp,setallSubCategoriesApp]=useState(allSubCategories)
+  //console.log(allSubCategoriesApp)
+
+  //----------------------
+useEffect(()=>{
+  axios.get(`${API_URL}/products`).then((res) =>{
+    //console.log(res.data)
+    setallproductsApp(res.data)
+    setproductBoyApp(res.data.filter(pr=>pr.category=== 'boys' ))
+    //console.log(productBoyApp)
+    setproductGirlApp(res.data.filter(pr=>pr.category=== 'girls' ))
+    //console.log(productGirlApp)
+    console.log(['all', ...new Set(res.data.map(pr=>pr.category))])
+    setallCategoriesApp(['all', ...new Set(res.data.map(pr=>pr.category))]) 
+    console.log(allCategoriesApp)
+  })
+},[])
 
   const alldata={
     allproductsApp,setallproductsApp,
@@ -34,9 +66,10 @@ function App() {
     isGirls,setisGirls,
     isBoys,setisBoys,
     genderStatus,setgenderStatus,
-    maincategoryApp,setmaincategoryApp
-
+    allMaincategoriesApp,setallMaincategoriesApp,
+    allSubCategoriesApp,setallSubCategoriesApp
   }
+
   return (
  <>
  <ProductsContext.Provider value={alldata}>
